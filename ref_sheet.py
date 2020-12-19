4dab16825ffd29cf04f5307a4c60ef35b0187cdb
hgo124578 <haya124578_124578>
2020-12-19 22:02:51 +0900

    added

---
+++ ref_sheet.py
@@ -0,0 +1,34 @@
+import gspread
+import os
+import json
+
+#ServiceAccountCredentials：Googleの各サービスへアクセスできるservice変数を生成します。
+from oauth2client.service_account import ServiceAccountCredentials
+
+#2つのAPIを記述しないとリフレッシュトークンを3600秒毎に発行し続けなければならない
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+#認証情報設定
+if os.path.isfile('key_cred.json'):
+    result = True
+#ダウンロードしたjsonファイル名をクレデンシャル変数に設定（秘密鍵、Pythonファイルから読み込みしやすい位置に置く）
+credentials = ServiceAccountCredentials.from_json_keyfile_name('key_cred.json', scope)
+
+#OAuth2の資格情報を使用してGoogle APIにログインします。
+gc = gspread.authorize(credentials)
+
+#共有設定したスプレッドシートキーを変数[SPREADSHEET_KEY]に格納する。
+SPREADSHEET_KEY = '1wrU0NzagU_G0KSH-F-mBDpH6gtiuj_k4Jt33wsUB-vU'
+
+#共有設定したスプレッドシートのシート1を開く
+worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
+
+#A1セルの値を受け取る
+import_value = int(worksheet.acell('A1').value)
+
+# #A1セルの値に100加算した値をB1セルに表示させる
+# export_value = import_value+100
+# worksheet.update_cell(1,2, export_value)
+
+with open("result.txt", mode='w') as f:
+    f.write(str(import_value))
